241d53a17c182de69d692b67a886eded8083a8df
Max <mthimm2@uic.edu>
2022-03-13 18:51:56 -0500

    created file for uart testing

--- n_integration.py
+++ n_integration.py
@@ -40,7 +40,7 @@ parser = argparse.ArgumentParser(description="Locate objects in a live camera st
                                  formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
                                  jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
-parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("output_URI", type=str, default="file://ride.mp4", nargs='?', help="URI of the output stream")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
---
+++ uart_on_Jetson.py
@@ -0,0 +1,82 @@
+'''
+The stock Jetson Nano starts a console on the ttyTHS1 serial port at startup through a service. 
+The script that starts the service is nvgetty.sh which launches getty. The script is located in /etc/systemd. 
+While this does not conflict with the script presented here, consider disabling the console if you are using the serial port to avoid conflicts. 
+Note that normal udev rules will be overridden by the console while the service is running. To disable the console:
+
+$ systemctl stop nvgetty
+$ systemctl disable nvgetty
+$ udevadm trigger
+# You may want to reboot instead - Not sure what this part means yet
+
+Install the py-serial library:
+$ sudo apt-get install python3-serial
+
+Pinout:
+Jetson Pins Used:
+Pin 6 - GND
+Pin 8 - D14 - TXD (Plugs into Arduino RX)
+Pin 10 - D15 - RXD (Plugs into Arduino TX)
+'''
+
+#!/usr/bin/python3
+import time
+import serial
+
+print("UART Demonstration Program")
+print("NVIDIA Jetson Nano Developer Kit")
+
+
+serial_port = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+)
+# Wait a second to let the port initialize
+time.sleep(1)
+
+# Make the data a queue
+# Not sure what more we're gonna have in here, but we can have a list of lists that we treat like a queue
+# Come time to write, we can prepare any number of 
+data = [[] * 2]
+
+try:
+    # Send a simple header
+    serial_port.write("UART Demonstration Program\r\n".encode())
+    serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
+    while True:
+        if serial_port.inWaiting() > 0:
+
+            # Take the data we've received and throw it into a queue
+            data.append(serial_port.read()) # data = serial_port.read()
+            print(data)
+
+            # This line just regurgitates what someone is typing on the other end in their demo
+            #serial_port.write(data)
+            
+            # if we get a carriage return, add a line feed too
+            # \r is a carriage return; \n is a line feed
+            # This is to help the tty program on the other end 
+            # Windows is \r\n for carriage return, line feed
+            # Macintosh and Linux use \n
+            #if data == "\r".encode():
+                # For Windows boxen on the other end
+                #serial_port.write("\n".encode())
+        
+        # The above block appears to take in one character at a time.
+        for char in data:
+            print(char, end = '')
+        print('')
+
+except KeyboardInterrupt:
+    print("Exiting Program")
+
+except Exception as exception_error:
+    print("Error occurred. Exiting Program")
+    print("Error: " + str(exception_error))
+
+finally:
+    serial_port.close()
+    pass

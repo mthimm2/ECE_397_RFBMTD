ce26c4e0110891061a4e7776d1e94e5835252c68
Max <mthimm2@uic.edu>
2022-03-17 22:07:54 -0500

    Begin integration with deepstream base. Add LRC, Distance approximation and object position

--- Deepstream_python/deepstream_test_folder/deepstream_python/ds_trafficcamnet.py
+++ Deepstream_python/deepstream_test_folder/deepstream_python/ds_trafficcamnet.py
@@ -40,42 +40,171 @@ def osd_sink_pad_buffer_probe(pad, info, u_data):
     ''' 
         
         Integration goes here
-    
+        TODO:
+            Closest object per segment (Width of BB as first metric. Then, if two objects are in same segment, compare BB center Y coords)
+                Distance function based on collected data (Width of obj in center segment of frame)
+                FDU LED Control 
+                    Includes system status LEDs
+            Accelerometer UART tie in for cutting off/saving ride recording
+            Other stuff TBD
     '''
 
-    # Retrieve batch metadata from the gst_buffer
-    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
-    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
+    # Get the batch of metadata from the buffer. Do not touch.
     batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+
+    '''
+    NvDsObjectMeta
+        class_id: int, Index of the object class infered by the primary detector/classifier
+        object_id: int, Unique ID for tracking the object. @ref UNTRACKED_OBJECT_ID indicates the object has not been tracked
+        detector_bbox_info: NvDsComp_BboxInfo, Holds a structure containing bounding box parameters of the object when detected by detector
+        tracker_bbox_info: NvDsComp_BboxInfo, Holds a structure containing bounding box coordinates of the object when processed by tracker
+
+    NvDsVehicleObject
+    '''
+
+    # We know that each frame coming in has the same dimensions for 720p capture
+    STANDARD_FRAME_WIDTH = 1280
+    STANDARD_FRAME_HEIGHT = 720
+
+    # This lets us statically define the LCR regions
+    # These numbers reflect that fact That we're looking behind us. Hence right is on the left of the frame.
+    RIGHT = (0, STANDARD_FRAME_WIDTH / 3)
+    CENTER = (STANDARD_FRAME_WIDTH / 3, 2 * (STANDARD_FRAME_WIDTH / 3))
+
+    #[frame zero, frame one]
+
     # Eric: I think L frame contains the current frame metadata for the objects detected and the objects being tracked.
+    # Get the list of frame meta objects from the batch meta object.
     l_frame = batch_meta.frame_meta_list
     while l_frame is not None:
+
+        # Lists for objects detected in LRC regions
+        left_det, center_det, right_det = [], [], []
+
         try:
-            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
-            # The casting is done by pyds.NvDsFrameMeta.cast()
-            # The casting also keeps ownership of the underlying memory
-            # in the C code, so the Python garbage collector will leave
-            # it alone.
+            # Cast current frame to an accessible metadata format
             frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
         except StopIteration:
             break
 
+        # Frame number from within the batch. Could be overall frame number
         frame_number = frame_meta.frame_num
+
+        # The number of objects is the number of bounding boxes
         num_rects = frame_meta.num_obj_meta
-        # Eric: The l object holds the metadata for the object detection list. The structure loops in a while loop until
-        # l_object data is loaded
+
+        # Get a list of the objects in frames' metadata
         l_obj = frame_meta.obj_meta_list
         while l_obj is not None:
             try:
-                # Casting l_obj.data to pyds.NvDsObjectMeta
+                # Cast l_obj.data to pyds.NvDsObjectMeta
                 obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
+
+                '''Could this cause pipeline issues if we try to cast the same thing twice?'''
+                # Gets the predicted direction of a given object within the frame
+                obj_direction = pyds.NvDsAnalyticsObjInfo.cast(l_obj.data).dirStatus
+
             except StopIteration:
                 break
             obj_counter[obj_meta.class_id] += 1
+            
             try:
                 l_obj = l_obj.next
             except StopIteration:
                 break
+
+            # Dive through casts to get the object's bounding box top left vertex, width, and height?
+            #obj_bb_coords = pyds.NvBbox_Coords.cast(pyds.NvDsComp_BboxInfo.cast(obj_meta.tracker_bbox_info))
+            obj_bb_coords = obj_meta.tracker_bbox_info.org_bbox_coords
+
+            # Assuming the above is correct, construct the bb for this object.
+            obj_tlv = (obj_bb_coords.left, obj_bb_coords.top)
+            obj_brv = (obj_bb_coords.left + obj_bb_coords.width, obj_bb_coords.top - obj_bb_coords.height)
+            obj_center_coords = ((obj_tlv[0] + obj_brv[0]) / 2, (obj_tlv[1] + obj_brv[1]) / 2)
+
+            # For the purpose of object distance calculation and position, we care mostly about bb width and bb center location
+            info_tuple = (obj_bb_coords.width, obj_center_coords, obj_meta.object_id)
+
+            # Based on where the center of the bb of the object is, we classify it as being in either the L,C, or R segment of the frame
+            '''
+                The away value assumes that the dirStatus field could have such a value.
+                No concrete examples were shown for what directions are possible.
+            '''
+            if obj_direction is not 'away':
+                if obj_center_coords[0] < RIGHT[1]:
+                    right_det.append(info_tuple)
+                elif obj_center_coords >= CENTER[0] and obj_center_coords < CENTER[1]:
+                    center_det.append(info_tuple)
+                else:
+                    left_det.append(info_tuple)
+            
+        '''Once we see these working, we can make this a function, since it's blatant copy and paste for each list'''
+        # This variable is taller than the frame, meaning the center of any bb must be below it
+        # This allows us to gradually look for the minimum bb center y coord in each list
+        Y_MIN = 721
+        l_min_buff, cent_min_buff, r_min_buff = [], [], []
+        for info_t in left_det:
+
+            # If the y coord of a bb's center is strictly lt Y_MIN, it must be closer by our standard
+            # Therefore, we can clear the list of all other entries and put the new lowest into the list
+            if info_t[1][1] < Y_MIN:
+                l_min_buff.clear()
+                l_min_buff.append(info_t)
+                Y_MIN = info_t[1][1]
+            
+            # We have to be careful of objects who's bbs are at a similar y coordinate, but aren't actually equidistant in reality
+            # A car that's far away, but in the center of a frame may have a similar bb center coord to a car that's near to us and in the center of the frame
+            # So we need to have a window of similarity in case we run into this situation
+            elif Y_MIN - info_t[1][1] <= 0.1 * Y_MIN:
+                l_min_buff.append(info_t)
+            else:
+                pass
+
+        # Center segment min y coord
+        for info_t in center_det:
+            
+            if info_t[1][1] < Y_MIN:
+                cent_min_buff.clear()
+                cent_min_buff.append(info_t)
+                Y_MIN = info_t[1][1]
+
+            elif Y_MIN - info_t[1][1] <= 0.1 * Y_MIN:
+                cent_min_buff.append(info_t)
+            
+            else:
+                pass
+
+        # Right segment min y coord
+        for info_t in right_det:
+    
+            if info_t[1][1] < Y_MIN:
+                r_min_buff.clear()
+                r_min_buff.append(info_t)
+                Y_MIN = info_t[1][1]
+            
+            elif Y_MIN - info_t[1][1] <= 0.1 * Y_MIN:
+                r_min_buff.append(info_t)
+
+            else:
+                pass
+
+        # Determine closes object in each frame
+        l_max_width = max([info_t[0] for info_t in l_min_buff]) if len(l_min_buff) > 0 else 0
+        c_max_width = max([info_t[0] for info_t in cent_min_buff]) if len(cent_min_buff) > 0 else 0
+        r_max_width = max([info_t[0] for info_t in r_min_buff]) if len(r_min_buff) > 0 else 0
+
+        '''
+            Do we need to have any special considerations for objects not in the center segment?
+            Is this where we make use of the distance coefficient to help compensate for the fish-eyeing?
+        '''
+        # Ratio of bb width to width of the frame
+        # Serves as a rudimentary form of how close an object is
+        l_coeff = l_max_width / STANDARD_FRAME_WIDTH
+        r_coeff = r_max_width / STANDARD_FRAME_WIDTH
+        c_coeff = c_max_width / STANDARD_FRAME_WIDTH
+
+        # Closest per segment known here
+            # Update FDU
 
         ''' 
             

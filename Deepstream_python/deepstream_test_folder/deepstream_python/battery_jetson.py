2a3de94eb3b3f71539e6a8f10eae7ab0bba325e0
Eric <36737239+ericm1030@users.noreply.github.com>
2022-03-25 12:16:13 -0500

    Update battery_jetson.py
    Updated bus declaration to avoid interference from GST bus message handler.

--- Deepstream_python/deepstream_test_folder/deepstream_python/battery_jetson.py
+++ Deepstream_python/deepstream_test_folder/deepstream_python/battery_jetson.py
@@ -4,33 +4,33 @@ import smbus
 import sys
 import time
 
-def readVoltage(bus):
+def readVoltage(bat_bus):
     address = 0x36
-    read = bus.read_word_data(address, 2)
+    read = bat_bus.read_word_data(address, 2)
     swapped = struct.unpack("<H", struct.pack(">H", read))[0]
     voltage = swapped * 1.25 / 1000 / 16
     return voltage
 
-def readCapacity(bus):
+def readCapacity(bat_bus):
     address = 0x36
-    read = bus.read_word_data(address, 4)
+    read = bat_bus.read_word_data(address, 4)
     swapped = struct.unpack("<H", struct.pack(">H", read))[0]
     capacity = swapped / 256
     return capacity
 
 if __name__ == "__main__":
-    bus = smbus.SMBus(1) # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+    bat_bus = smbus.SMBus(1) # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
     while True:
         print("******************")
-        print("Voltage:%5.2fV" % readVoltage(bus))
+        print("Voltage:%5.2fV" % readVoltage(bat_bus))
 
-        print("Battery:%5i%%" % readCapacity(bus))
+        print("Battery:%5i%%" % readCapacity(bat_bus))
 
-        if readCapacity(bus) == 100:
+        if readCapacity(bat_bus) == 100:
             print("Battery FULL")
 
-        if readCapacity(bus) < 20:
+        if readCapacity(bat_bus) < 20:
                 print("Battery LOW")
 				
         print("******************")

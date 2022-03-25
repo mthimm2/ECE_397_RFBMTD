fe4e62cf9cac1834be72f86c639f4d8635c17200
Eric Moravek <ericm1030@gmail.com>
2022-03-25 15:05:44 -0500

    removed redunant statements

--- deepstream-test2-filesink/deepstream_test2_csi_filesink.py
+++ deepstream-test2-filesink/deepstream_test2_csi_filesink.py
@@ -36,9 +36,6 @@ from common.is_aarch_64 import is_aarch64
 from common.bus_call import bus_call
 import pyds
 
-# Battery Integration Imports
-import smbus
-
 
 # To Print The dot graph Gstreamer pipeline
 os.environ["GST_DEBUG_DUMP_DOT_DIR"] = "/tmp"

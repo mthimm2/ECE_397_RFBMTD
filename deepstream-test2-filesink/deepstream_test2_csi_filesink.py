ff6ad62291f04ee90a295cc7381f2a6acbbe23e3
Eric Moravek <ericm1030@gmail.com>
2022-03-25 17:51:14 -0500

    bat bus dec

--- deepstream-test2-filesink/deepstream_test2_csi_filesink.py
+++ deepstream-test2-filesink/deepstream_test2_csi_filesink.py
@@ -38,6 +38,8 @@ import pyds
 
 # Import Battery Module
 from battery_module import *
+bat_bus = smbus.SMBus(1)
+
 
 # Import Uart Communication Module
 from uart_module import *
@@ -255,7 +257,6 @@ def osd_sink_pad_buffer_probe(pad,info,u_data):
         r_data  = EncodeDistanceData(r_coeff, CLOSE_COEFF, MED_COEFF, FAR_COEFF)
 
         # Battery functions 
-        bat_bus = smbus.SMBus(1)    # TODO: check whether or not to leave this here or before loop
         battery_cap = readCapacity(bat_bus)
         b_data = ""
         

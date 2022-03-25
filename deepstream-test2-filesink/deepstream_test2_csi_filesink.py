fa2243ef7e5601864d6a87b1132dd35118b27e62
Eric Moravek <ericm1030@gmail.com>
2022-03-25 18:26:33 -0500

    commit

--- deepstream-test2-filesink/deepstream_test2_csi_filesink.py
+++ deepstream-test2-filesink/deepstream_test2_csi_filesink.py
@@ -288,6 +288,9 @@ def osd_sink_pad_buffer_probe(pad,info,u_data):
         if history_dict[obj_meta.object_id]['brv'][0] == 1280 and history_dict[obj_meta.object_id]['delta_h'] > 0:
             uart_transmission.send("1" + c_data + r_data + o_data)
 
+        l_data=1
+        c_data=2
+        r_data=3
         # Cyclist's right side [object is passing close right (cyclist rear POV)]
         elif history_dict[obj_meta.object_id]['tlv'][0] == 0 and history_dict[obj_meta.object_id]['delta_h'] > 0:
             uart_transmission.send(l_data + c_data + "1" + o_data)
